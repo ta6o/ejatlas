a8ecb32af4049cfbf3c89bb606d82f54c69cf168
Yakup Cetinkaya <yakup.cetinkaya@gmail.com>
2015-03-05 13:19:38 +0200

    embed functional

--- admin/app.rb
+++ admin/app.rb
@@ -11,7 +11,7 @@ class Admin < Padrino::Application
   #
   # set :raise_errors, true        # Raise exceptions (will stop application) (default for test)
   # set :dump_errors, true         # Exception backtraces are written to STDERR (default for production/development)
-  set :show_exceptions, true     # Shows a stack trace in browser (default for development)
+  # set :show_exceptions, true     # Shows a stack trace in browser (default for development)
   # set :logging, true             # Logging in STDOUT for development and file for production (default only for development)
   # set :public_folder, "foo/bar"  # Location for static assets (default root/public)
   # set :reload, false             # Reload application files (default in development)
