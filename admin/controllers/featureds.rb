3678a7c76b1f2ca507486840f5c29d06405820f1
yakup <yakup.cetinkaya@gmail.com>
2015-06-06 20:08:08 +0300

    aha

--- admin/controllers/featureds.rb
+++ admin/controllers/featureds.rb
@@ -53,7 +53,7 @@ Admin.controllers :featureds do
       @contained[$attrhash.select{|k,v| v == val}.keys.first] = val
     end
     begin
-      @followed = (Admin.filter(@featured.filter).map{|i| begin Conflict.select('id, slug, name, approval_status, features').find(i['_id'].to_i rescue nil end)}-[nil]).sort{|a,b| a.slug <=> b.slug}
+      @followed = (Admin.filter(@featured.filter).map{|i| begin Conflict.select('id, slug, name, approval_status, features').find(i['_id'].to_i) rescue nil end}-[nil]).sort{|a,b| a.slug <=> b.slug}
     rescue => e
       puts "#{featured.name} | #{e}"
       @followed = (Admin.old_filter(@featured.filter) || []).sort{|a,b| a.slug <=> b.slug})
