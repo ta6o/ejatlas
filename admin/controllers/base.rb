b2ac5d500da2d7ba9065a99326ab8ad93af340a1
yakup cetinkaya <yakup.cetinkaya@gmail.com>
2016-05-19 18:26:52 +0300

    hayde

--- admin/controllers/base.rb
+++ admin/controllers/base.rb
@@ -292,7 +292,7 @@ Admin.controller do
     con = Featured.find_slug(params[:slug])
     pass unless con
     @markerinfo = con.conflicts_marker
-    @load = @markerinfo.length > 4096 ? nil : con.conflicts_link
+    @load = con.conflicts_link.length > (2**16) ? nil : con.conflicts_link
     @name = con.name
     @description = con.description
     @id = con.id
