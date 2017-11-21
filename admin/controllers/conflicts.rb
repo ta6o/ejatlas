4530a7f0ce6b66041e7b10b8f557b3f66bf45525
yakup <yakup.cetinkaya@gmail.com>
2017-11-21 22:52:57 +0800

    emailz

--- admin/controllers/conflicts.rb
+++ admin/controllers/conflicts.rb
@@ -352,11 +352,12 @@ Admin.controllers :conflicts do
   end
 
   post :msg_create do
+    @conflict = Conflict.find(params["cid"])
     if current_account and ( ["admin","editor"].include?(current_account.role) or @conflict.account_id == current_account.id or @conflict.conflict_accounts.map(&:account_id).include?(current_account.id))
       ConflictMessage.create :conflict_id => params["cid"], :account_id => params["aid"], :content => params["content"]
       return "ok"
     else
-      redirect to '/sessions/login'
+      pass
     end
   end
 
