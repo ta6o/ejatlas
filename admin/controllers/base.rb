6ca0f41b58e5e3ea15213c124da76435ecdd9773
yakup <yakup.cetinkaya@gmail.com>
2018-02-14 13:54:00 +0300

    approve!

--- admin/controllers/base.rb
+++ admin/controllers/base.rb
@@ -115,6 +115,12 @@ Admin.controller do
 
   get :conflict, :with => :slug do
     c = Conflict.find_slug(params[:slug])
+    if c.approval_status != "approved"
+      if current_account and ["admin","editor"].include?(current_account.role) or @conflict.account_id == current_account.id or @conflict.conflict_accounts.map(&:account_id).include?(current_account.id)
+      else
+        pass
+      end
+    end
     #last_modified c.updated_at
     pass unless c
     @markerinfo = c.accurate_marker
