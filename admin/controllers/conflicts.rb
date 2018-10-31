5f0cd49be813a8ed1c517dd32547d0ee8d179096
yakup <yakup.cetinkaya@gmail.com>
2018-10-31 22:52:46 +0300

    getting conflict list from esearch

--- admin/controllers/conflicts.rb
+++ admin/controllers/conflicts.rb
@@ -201,7 +201,7 @@ Admin.controllers :conflicts do
   get :index do
     if current_account
       if ["admin","editor"].include? current_account.role
-        @conflictos = Admin.filter("{}", true, 'id,name,slug,account_id,category_id,modified_at,approval_status'.split(","),false)
+        @conflictos = Admin.filter("{}", true, 'id,name,slug,account_id,category_id,modified_at,approval_status'.split(","),false).map{|x| x["_source"]}
         @accounts = Admin.filter("{}", true, 'id,name'.split(","),false,'account').map{|x| [x["_source"]["id"],x["_source"]["name"]]}.to_h
         @categories = Category.all.map {|c| [c.id,c.name]}.to_h
         @conflictos.sort_by! {|c| ( c["modified_at"] || Time.now ) }
