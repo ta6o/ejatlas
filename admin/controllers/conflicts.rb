18f6f9dab34589a911b121ee845b061ea0c16eb4
yakup <yakup.cetinkaya@gmail.com>
2019-03-20 12:43:17 +0100

    latlon validation

--- admin/controllers/conflicts.rb
+++ admin/controllers/conflicts.rb
@@ -306,8 +306,8 @@ Admin.controllers :conflicts do
       @conflict = Conflict.find(params[:id])
       if ["admin","editor"].include?(current_account.role) or @conflict.account_id == current_account.id or @conflict.conflict_accounts.map(&:account_id).include?(current_account.id)
         @cjson = Conflict.where(approval_status: 'approved').order('slug').select('name,id').map{|j|{:id=>j['id'],:value=>j['name']}}.to_json
-        @lat = @conflict.lat == "" ? nil : @conflict.lat
-        @lon = @conflict.lon == "" ? nil : @conflict.lon
+        @lat = @conflict.lat.match(/^-?\d+\.?\d*$/) ? @conflict.lat : nil
+        @lon = @conflict.lon.match(/^-?\d+\.?\d*$/) ? @conflict.lon : nil
         @saves = []
         CSV.read("#{Dir.pwd}/misc/saves.csv").each do |row|
           @saves << row if row[2] == @conflict.id.to_s
