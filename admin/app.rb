2c9264f3fe69afa94dfe377eece4ebd8842d0538
yakup <yakup.cetinkaya@gmail.com>
2019-05-23 13:50:45 +0200

    puts's

--- admin/app.rb
+++ admin/app.rb
@@ -412,7 +412,7 @@ class Admin < Padrino::Application
   def self.filter filter, all_if_empty=true, stored_fields=[], approved=true, type='conflict'
     return [] if !all_if_empty and ["{}","",nil].include?(filter)
     if type == "conflict"
-      #puts JSON.pretty_generate(JSON.parse(filter))
+      #puts JSON.pretty_generate(JSON.parse(filter)).yellow
       if approved
         filter = Admin.elasticify( { bool: { must: { term: { approval_status: "approved" }}, filter: { bool: JSON.parse( filter ) }}} )
       else
