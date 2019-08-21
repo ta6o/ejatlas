56dbeb5eec63d104f0c5a1a23d78d9ea7df6d45a
yakup <yakup.cetinkaya@gmail.com>
2019-08-21 21:40:48 +0400

    last_update

--- admin/controllers/base.rb
+++ admin/controllers/base.rb
@@ -158,7 +158,7 @@ Admin.controller do
     @slug = c.slug
     @desc = c.description
     @headline = c.headline
-    @modified = c.modified_at ? "#{I18n.t('f.conflict.last_update',:locale=>loc)}: #{I18n.l(c.modified_at)}" : "&nbsp;"
+    @modified = c.modified_at ? "#{I18n.t('f.conflict.last_update',:locale=>I18n.locale)}: #{I18n.l(c.modified_at)}" : "&nbsp;"
     @title = c.title
     @ogimage = c.images.first.file.url if c.images.any?
     @cid = c.id 
