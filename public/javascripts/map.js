6ef136e1b6af4e5460ab2d5d34e685bf16f2b587
yakup <yakup.cetinkaya@gmail.com>
2019-09-19 12:51:11 +0300

    search box

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -951,7 +951,6 @@ function showVector(v) {
     console.log('fail - no pn');
     return 0
   }
-  console.log(pn)
   vect = $.grep(vectorinfo,function(i,n){return i.vector_datum.name == pn});
   console.log(vect)
   if(vect.length == 0) {
