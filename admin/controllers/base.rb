97a66fbf1af208010e5ec1cf1f744c536293f43d
yakup <yakup.cetinkaya@gmail.com>
2020-04-21 13:21:16 +0300

    no default_ locale

--- admin/controllers/base.rb
+++ admin/controllers/base.rb
@@ -150,13 +150,13 @@ Admin.controller do
   end
 
   get :index do
-    ca = Cached.where(:locale=>I18n.default_locale).first
+    ca = Cached.where(:locale=>I18n.locale).first
     pass unless ca
     #last_modified ca.updated_at
     @filterform = {}
     @filterform = JSON.parse(ca.filterdata) if ca
     @filter = render "base/filter", :layout => false
-    @markercount = ConflictText.where(:approval_status=> 'approved',:locale=>I18n.default_locale).count
+    @markercount = ConflictText.where(:approval_status=> 'approved',:locale=>I18n.locale).count
     countries = ca.countries ? JSON.parse(ca.countries) : []
     companies = ca.companies ? JSON.parse(ca.companies) : []
     commodities = ca.commodities ? JSON.parse(ca.commodities) : []
@@ -173,7 +173,7 @@ Admin.controller do
 
   get :conflict, :with => :slug do
     c = Conflict.find_slug(params[:slug].downcase)
-    if I18n.locale != I18n.default_locale and ConflictText.where(:conflict_id=>c.id, :locale=>I18n.locale).empty? and false
+    if I18n.locale != I18n.locale and ConflictText.where(:conflict_id=>c.id, :locale=>I18n.locale).empty? and false
       Admin.tx_conflict c.conflict_texts.order(:created_at).first, I18n.locale, false, true
     end
     #last_modified c.updated_at
@@ -248,7 +248,7 @@ Admin.controller do
   get "/:model" do
     browseinfo = {"country"=>"countries","company"=>"companies","commodity"=>"commodities","type"=>"types"}
     pass unless browseinfo.has_key?(params[:model])
-    ca = Cached.where(:locale=>I18n.default_locale).first
+    ca = Cached.where(:locale=>I18n.locale).first
     @filterform = JSON.parse(ca.filterdata)
     @filter = render "base/filter", :layout => false
     @markercount = JSON.parse(ca.conflicts_marker).length
@@ -267,7 +267,7 @@ Admin.controller do
   end
 
   get :country, :with => :slug do
-    ca = Cached.where(:locale=>I18n.default_locale).first
+    ca = Cached.where(:locale=>I18n.locale).first
     con = Country.find_slug(params[:slug])
     con = Region.find_slug(params[:slug]) unless con
     pass unless con
@@ -299,7 +299,7 @@ Admin.controller do
   end
 
   get :company, :with => :slug do
-    ca = Cached.where(:locale=>I18n.default_locale).first
+    ca = Cached.where(:locale=>I18n.locale).first
     con = Company.find_slug(params[:slug])
     pass unless con
     @filterform = JSON.parse(ca.filterdata)
@@ -321,7 +321,7 @@ Admin.controller do
   end
 
   get :institution, :with => :slug do
-    ca = Cached.where(:locale=>I18n.default_locale).first
+    ca = Cached.where(:locale=>I18n.locale).first
     con = Supporter.find_slug(params[:slug])
     pass unless con
     @filterform = JSON.parse(ca.filterdata)
@@ -342,7 +342,7 @@ Admin.controller do
   end
 
   get :commodity, :with => :slug do
-    ca = Cached.where(:locale=>I18n.default_locale).first
+    ca = Cached.where(:locale=>I18n.locale).first
     @filterform = JSON.parse(ca.filterdata)
     @filter = render "base/filter", :layout => false
     con = Product.find_slug(params[:slug])
@@ -361,7 +361,7 @@ Admin.controller do
   end
 
   get :type, :with => :slug do
-    ca = Cached.where(:locale=>I18n.default_locale).first
+    ca = Cached.where(:locale=>I18n.locale).first
     @filterform = JSON.parse(ca.filterdata)
     @filter = render "base/filter", :layout => false
     con = Type.find_slug(params[:slug])
@@ -385,7 +385,7 @@ Admin.controller do
   get 'country-of-company', :with => :slug do
     con = Country.find_slug(params[:slug])
     pass unless con
-    ca = Cached.where(:locale=>I18n.default_locale).first
+    ca = Cached.where(:locale=>I18n.locale).first
     @filterform = JSON.parse(ca.filterdata)
     @filter = render "base/filter", :layout => false
     ##last_modified con.updated_at
@@ -405,7 +405,7 @@ Admin.controller do
   get 'country-of-institution', :with => :slug do
     con = Country.find_slug(params[:slug])
     pass unless con
-    ca = Cached.where(:locale=>I18n.default_locale).first
+    ca = Cached.where(:locale=>I18n.locale).first
     ##last_modified con.updated_at
     @markerinfo = con.supporters_marker
     @name = "Financial Institutions from #{con.name}"
