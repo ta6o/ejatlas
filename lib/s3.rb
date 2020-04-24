98d032e5d48d17b8ef40f56bb996c4490eec2b4e
yakup <yakup.cetinkaya@gmail.com>
2020-04-24 18:07:48 +0300

    doc upload response

--- lib/s3.rb
+++ lib/s3.rb
@@ -66,11 +66,11 @@ end
 class DocumentUploader < CarrierWave::Uploader::Base
   storage :file
   def store_dir
-    "#{$filedir}/docs/#{self.conflict_id}"
+    "#{$filedir}/docs/#{self.file.conflict_id}"
   end
 
   def url
-    "#{$fileurl}/docs/#{self.conflict_id}/#{self.file.filename}"
+    "#{$fileurl}/docs/#{self.file.conflict_id}/#{self.file.filename}"
   end
 
   def cache_dir
