bf6ff57ebe22cb54be2b8f6b767027c93785bdb1
yakup <yakup.cetinkaya@gmail.com>
2020-12-09 11:19:22 +0300

    auto_tx not verbose

--- admin/controllers/base.rb
+++ admin/controllers/base.rb
@@ -260,7 +260,7 @@ Admin.controller do
     @conflict = Conflict.find_slug(params[:slug].downcase)
     pass unless @conflict
     if I18n.locale != I18n.default_locale and params.has_key?("translate") and ConflictText.where(:conflict_id=>@conflict.id, :locale=>I18n.locale).empty?
-      Admin.tx_conflict @conflict.conflict_texts.order(:created_at).first, I18n.locale, false, true
+      Admin.tx_conflict @conflict.conflict_texts.order(:created_at).first, I18n.locale, false
     end
     pass unless ["approved","modified","auto_tx"].include?(@conflict.approval_status) or (current_account and current_account.contributor?(@conflict))
     @markerinfo = @conflict.accurate_marker
